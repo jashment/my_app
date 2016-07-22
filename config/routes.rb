fa22a39dc9b03f77f55c09f24792943035e3ae9d
Jordan <jashment22@gmail.com>
2016-07-22 21:49:36 +0000

    scaffold crud articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :articles
   root to: 'pages#index'
 
   get 'pages/contact'
 
   get 'pages/about'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
